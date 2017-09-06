16ab954334ee1bcea81e8162602e110b955fc87c
Evywell <leduc.axel.pro@gmail.com>
2017-09-06 16:50:21 +0200

    First commit

---
+++ www/js/my-app.js
@@ -0,0 +1,61 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+/**
+ * Action sur le bouton du sommaire
+ */
+var sommaire = $$('.sommaire');
+$$('.btn-sommaire').on('click', function () {
+    sommaire.toggleClass('open');
+});
+
+/**
+ * Actions sur les titres des articles
+ */
+$$('.article').on('click', function(e) {
+    e.preventDefault();
+    // Afficher / Masquer les boutons
+    var el = this.children[1];
+    el.classList.toggle('hidden');
+})
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
